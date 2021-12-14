7ba310ff373fc66402049ef04074564ae10d0891
Алексей <mr.nail.brain13@gmail.com>
2021-12-15 01:30:39 +0300

    Lesson-3 my tests

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
-    }
-})
+  data() {
+    return {
+      product: 'Socks',
+      image: './assets/images/socks_green.jpg',
+      desc: 'Vue-tests',
+      url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
+    };
+  },
+});
