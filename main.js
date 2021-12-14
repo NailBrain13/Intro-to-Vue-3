f4f7450ffab8bad2322e09af5af6d66beaf13123
Алексей <mr.nail.brain13@gmail.com>
2021-12-15 01:49:13 +0300

    L4-my tests

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
-        }
-    }
-})
+  data() {
+    return {
+      product: 'Socks',
+      image: './assets/images/socks_blue.jpg',
+      isAvailable: true,
+      inventory: 8,
+      onSale: true,
+      saleMes: 'Sale! NOW',
+    };
+  },
+});
